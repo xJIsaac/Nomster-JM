70bd63432e57d1cee66a83c3ad0894f411251f44
Jaime M <j7isaac@gmail.com>
2015-11-11 05:29:02 +0000

    Configured the submit button to create a new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,21 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.paginate(:page => params[:page], :per_page => 4)
+		@places = Place.paginate(:page => params[:page], :per_page => 3)
 	end
 
 	def new
 		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
 	end
 
 end
